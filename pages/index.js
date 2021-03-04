30dfb51dff2ab4ac36df4b66b70fee55136e55d3
S-Brand5136 <Brandon.Shemilt@live.co.uk>
2021-03-03 21:16:52 -0700

    add front page image

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,24 @@
 import Head from "next/head";
+import Image from "next/image";
 import Sitenav from "../components/Sitenav";
 import styles from "../styles/Home.module.css";
 
+// React-bootstrap
+import { Container, Col, Row } from "react-bootstrap";
+
 export default function Home() {
-  return <div></div>;
+  return (
+    <Container fluid>
+      <Row>
+        <Col xs={12} md={6}>
+          <Image
+            src="../images/hamburger"
+            alt="Picture of Menu Item"
+            layout="fill"
+          />
+        </Col>
+        <Col xs={12} md={6}></Col>
+      </Row>
+    </Container>
+  );
 }
